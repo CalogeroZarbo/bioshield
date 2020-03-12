6d0c6550fc7ebeb2f02a7bc92db0a53c811bc9c9
Calogero Zarbo <calogero.zarbo@docebo.com>
2020-03-12 18:53:13 +0100

    input params update

--- train_model_torch.py
+++ train_model_torch.py
@@ -192,8 +192,8 @@ def add_argument():
                         help='use CPU in case there\'s no GPU support')
     parser.add_argument('--use_ema', default=False, action='store_true',
                         help='whether use exponential moving average')
-    parser.add_argument('-b', '--batch_size', default=32, type=int,
-                        help='mini-batch size (default: 32)')
+    #parser.add_argument('-b', '--batch_size', default=32, type=int,
+    #                    help='mini-batch size (default: 32)')
     parser.add_argument('-e', '--epochs', default=30, type=int,
                         help='number of total epochs (default: 30)')
     parser.add_argument('--local_rank', type=int, default=-1,
@@ -211,6 +211,10 @@ def add_argument():
                         help='Bucket size for hashing') # 8 
     parser.add_argument('--depth', type=int, default=12,
                         help='number of hidden layers') # 12
+    parser.add_argument('--validate_every', type=int, default=10,
+                        help='Frequency of validation') # 12
+    parser.add_argument('--save_every', type=int, default=10,
+                        help='Frequency of saving checkpoint') # 12
 
     parser.add_argument('--path_to_file_tr', default='./gen_to_mol_tr.csv', help='Trainig file') 
     parser.add_argument('--path_to_file_ts', default='./gen_to_mol_ts.csv', help='Testing file') 
@@ -218,7 +222,7 @@ def add_argument():
     parser.add_argument('--max_len_mol', type=int, default=2048, help='Max symbols for Canonical SMILES') 
     parser.add_argument('--num_examples_tr', type=int, default=1024, help='Max number of samples TR') 
     parser.add_argument('--num_examples_ts', type=int, default=1024, help='Max number of samples TS') 
-    parser.add_argument('--train_batch_size', default=8, help='Batch size') 
+    parser.add_argument('--train_batch_size', type=int,default=8, help='Batch size') 
     parser.add_argument('--heads', type=int, default=8, help='Heads')
     parser.add_argument('--n_hashes', type=int, default=4, help='Number of hashes - 4 is permissible per author, 8 is the best but slower') 
 
@@ -235,7 +239,7 @@ def main():
     max_len_mol = cmd_args.max_len_mol
     num_examples_tr = cmd_args.num_examples_tr
     num_examples_ts = cmd_args.num_examples_ts
-    batch_size = cmd_args.batch_size
+    #batch_size = cmd_args.batch_size
     train_batch_size = cmd_args.train_batch_size
     epochs = cmd_args.epochs
     emb_dim = cmd_args.emb_dim
@@ -246,14 +250,17 @@ def main():
     n_hashes = cmd_args.n_hashes
     ff_chunks = cmd_args.ff_chunks
     attn_chunks = cmd_args.attn_chunks
-    use_apex = cmd_args.use_apex
+    validate_every = cmd_args.validate_every
+    save_every = cmd_args.save_every
 
     MAX_LENGTH_GEN = max_len_gen # 32768
     MAX_LENGTH_MOL = max_len_mol # 2048
     NUM_EXAMPLES_TR = num_examples_tr # 1024
     NUM_EXAMPLES_TS = num_examples_ts # 1024
-    BATCH_SIZE = batch_size # 32
+    #BATCH_SIZE = batch_size # 32
     N_EPOCHS = epochs # 10
+    VALIDATE_EVERY = validate_every
+    SAVE_EVERY = save_every
 
     VIR_SEQ_LEN = MAX_LENGTH_GEN # input_lang.max_len if (input_lang.max_len % 2) == 0  else input_lang.max_len + 1 # 32000
     MOL_SEQ_LEN = MAX_LENGTH_MOL # output_lang.max_len if (output_lang.max_len % 2) == 0  else output_lang.max_len + 1 # ??
@@ -275,8 +282,8 @@ def main():
             pair = self.data[index]
             #print('src:',pair[0])
             #print('\n\ntrg:',pair[1])
-            src = torch.tensor(indexesFromSentence(self.src_lang,pair[0])).long()
-            trg = torch.tensor(indexesFromSentence(self.trg_lang,pair[1])).long()
+            src = torch.tensor(indexesFromSentence(self.src_lang,pair[0]), dtype=torch.long)
+            trg = torch.tensor(indexesFromSentence(self.trg_lang,pair[1]), dtype=torch.long)
             # print('src:', src)
             # print('trg:', trg)
             return src,trg
@@ -289,7 +296,7 @@ def main():
 
     encoder = ReformerLM(
         num_tokens = input_lang.n_words,
-        emb_dim = emb_dim,#128,
+        # emb_dim = emb_dim,#128,
         dim = dim,#512,
         bucket_size = bucket_size, # 16,
         depth = depth, # 6,
@@ -299,16 +306,16 @@ def main():
         ff_chunks = ff_chunks, #400,      # number of chunks for feedforward layer, make higher if there are memory issues
         attn_chunks = attn_chunks, #16,    # process lsh attention in chunks, only way for memory to fit when scaling to 16k tokens
         weight_tie = True,
-        #axial_position_emb = True,
-        #axial_position_shape = (256, 128),  # the shape must multiply up to the max_seq_len (256 x 128 = 32768)
-        #axial_position_dims = (int(dim/2), int(dim/2)),  # the dims must sum up to the model dimensions (512 + 512 = 1024)
-        fixed_position_emb = True,
+        axial_position_emb = True,
+        axial_position_shape = (256, 128),  # the shape must multiply up to the max_seq_len (256 x 128 = 32768)
+        axial_position_dims = (int(dim/2), int(dim/2)),  # the dims must sum up to the model dimensions (512 + 512 = 1024)
+        #fixed_position_emb = True,
         return_embeddings = True # return output of last attention layer
     ).cuda()
 
     decoder = ReformerLM(
         num_tokens = target_lang.n_words,
-        emb_dim = emb_dim, # 128,
+        # emb_dim = emb_dim, # 128,
         dim = dim, # 512,
         bucket_size = bucket_size, #16,
         depth = depth, #6,
@@ -317,20 +324,16 @@ def main():
         ff_chunks = ff_chunks, # 400,      # number of chunks for feedforward layer, make higher if there are memory issues
         attn_chunks = attn_chunks, # 16,    # process lsh attention in chunks, only way for memory to fit when scaling to 16k tokens
         max_seq_len = MOL_SEQ_LEN,
-        #axial_position_emb = True,
-        #axial_position_shape = (64, 32),  # the shape must multiply up to the max_seq_len (64 x 32 = 2048)
-        #axial_position_dims = (int(dim/2), int(dim/2)),  # the dims must sum up to the model dimensions (512 + 512 = 1024)
-        fixed_position_emb = True,
+        axial_position_emb = True,
+        axial_position_shape = (64, 32),  # the shape must multiply up to the max_seq_len (64 x 32 = 2048)
+        axial_position_dims = (int(dim/2), int(dim/2)),  # the dims must sum up to the model dimensions (512 + 512 = 1024)
+        #fixed_position_emb = True,
         weight_tie = False,
         causal = True
     ).cuda()
 
     encoder_optimizer = RangerLars(encoder.parameters()) # torch.optim.Adam(encoder.parameters(), lr=learning_rate)
     decoder_optimizer = RangerLars(decoder.parameters()) # torch.optim.Adam(decoder.parameters(), lr=learning_rate)
-
-    if use_apex:
-        encoder, encoder_optimizer = amp.initialize(encoder, encoder_optimizer, opt_level='O1')
-        decoder, decoder_optimizer = amp.initialize(decoder, decoder_optimizer, opt_level='O1')
 
     encoder = TrainingWrapper(encoder).cuda()
 
@@ -343,8 +346,6 @@ def main():
     decoder_engine, decoder_optimizer, _, _ = deepspeed.initialize(args=cmd_args, model=decoder, optimizer=decoder_optimizer, model_parameters=encoder_params, dist_init_required=False)
    
     # training
-    VALIDATE_EVERY = 1
-    SAVE_EVERY = 1
     SAVE_DIR = './saved_model/'
 
     _, encoder_client_sd = encoder_engine.load_checkpoint(SAVE_DIR+'encoder/', None)
@@ -394,7 +395,8 @@ def main():
                 #    mini_batch.append(pair)
             print(f'\tValidation Loss: AVG: {np.mean(val_loss)}, MEDIAN: {np.median(val_loss)}, STD: {np.std(val_loss)} ')
 
-        if i % SAVE_EVERY:
+        if i % SAVE_EVERY == 0:
+            print('Saving...')
             if encoder_client_sd:
                 encoder_client_sd['step'] = i
             if decoder_client_sd:
@@ -404,59 +406,5 @@ def main():
             encoder_engine.save_checkpoint(SAVE_DIR+'encoder/', ckpt_id)# client_sd = encoder_client_sd)
             decoder_engine.save_checkpoint(SAVE_DIR+'decoder/', ckpt_id)# client_sd = decoder_client_sd)
 
-        # if i % GENERATE_EVERY == 0:
-        #     model.eval()
-        #     inp = random.choice(val_dataset)[:-1]
-        #     prime = decode_tokens(inp)
-        #     print(f'%s \n\n %s', (prime, '*' * 100))
-
-        #     sample = model.generate(inp.cuda(), GENERATE_LENGTH)
-        #     output_str = decode_tokens(sample)
-        #     print(output_str)
-
-    ## Old training
-    # ret = trainIters(encoder, decoder, N_EPOCHS, pairs, input_lang, target_lang,print_every=1, batch_size=BATCH_SIZE, use_apex=use_apex)
-
-    '''
-    # INPUT DEFINITION
-    criterion = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
-
-    enc_optimizer = RangerLars(encoder.parameters()) # you can create 
-    dec_optimizer = RangerLars(decoder.parameters())
-
-    for epoch in range(N_EPOCHS):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            enc_optimizer.zero_grad()
-            dec_optimizer.zero_grad()
-
-            # forward + backward + optimize
-            enc_keys = encoder(inputs)               # (1, 4096, 1024)
-            outputs = decoder(labels[:, :-1], keys = enc_keys)   # (1, 4096, 20000)
-
-            loss = criterion(outputs.tranpose(1, 2), labels[:, 1:])
-            loss.backward()
-
-            enc_optimizer.step()
-            dec_optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-    print('Finished Training')
-
-
-    enc_keys = encoder(x)               # (1, 4096, 1024)
-    yo = decoder(yi, keys = enc_keys)   # (1, 4096, 20000)
-    '''
 if __name__ == '__main__':
     main()
